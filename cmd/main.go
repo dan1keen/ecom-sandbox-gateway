9b94f11aa4f2576561c35a6786388c507d018db6
danik <dan1ar3110@gmail.com>
2026-03-09 19:51:59 +0500

    fixes

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"gateway/config"
 	internalHttp "gateway/internal/http"
 	"gateway/server"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
@@ -20,35 +13,8 @@ func main() {
 
 	srv := server.NewHTTPServer(router, cfg.Port, cfg.ReadTimeout, cfg.WriteTimeout, cfg.IdleTimeout)
 
-	startServer(srv, cfg)
+	server.StartServer(srv, cfg)
 
 	// --- Graceful shutdown ---
-	waitForShutdown(srv, cfg)
-}
-
-func startServer(srv *http.Server, cfg *config.Config) {
-	go func() {
-		log.Printf("Server running on port %s (%s mode)", cfg.Port, cfg.Env)
-		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			log.Fatalf("Could not start server: %v", err)
-		}
-	}()
-}
-
-func waitForShutdown(srv *http.Server, cfg *config.Config) {
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-	defer signal.Stop(quit)
-
-	<-quit
-	log.Println("Shutdown signal received")
-
-	ctx, cancel := context.WithTimeout(context.Background(), cfg.ShutdownTimeout)
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v", err)
-	}
-
-	log.Println("Server exited gracefully")
+	server.WaitForShutdown(srv, cfg)
 }
